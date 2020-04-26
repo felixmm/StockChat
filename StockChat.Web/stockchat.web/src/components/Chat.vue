33684c3636ee270e860405d54fe344b6446654ef
Felix Martinez <f.martinez.espinal@gmail.com>
2020-04-26 09:55:10 -0400

    Adding bootstrap and styling to chat. Adding functionality to scroll to bottom on new message

--- StockChat.Web/stockchat.web/src/components/Chat.vue
+++ StockChat.Web/stockchat.web/src/components/Chat.vue
@@ -1,39 +1,63 @@
 <template>
-<div>
-    <div>
-        <ul>
-            <li :key="message.date" v-for="message in messageList">
-                {{ message.text }}
-            </li>
-        </ul>
+  <div class="container">
+    <div class="row">
+      <h1>Stock Chat</h1>
+      <ul ref="chatList" class="list-group list-group-flush col-12 chat-list">
+        <li
+          class="list-group-item text-left"
+          :key="message.date"
+          v-for="message in messageList"
+        >{{ new Date(message.date).getHours()}}:{{ new Date(message.date).getMinutes()}}:{{ new Date(message.date).getSeconds()}}: {{ message.text }}</li>
+      </ul>
     </div>
-    <div>
-        <input v-model="newMessage" v-on:keyup.enter="sendMessage" />
-        <button v-on:click="sendMessage">Send</button>
+    <div class="row mt-3 mb-5">
+      <input
+        class="col-10"
+        placeholder="chat here"
+        v-model="newMessage"
+        v-on:keyup.enter="sendMessage"
+      />
+      <button class="col-2" v-on:click="sendMessage">Send</button>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Chat',
-    data: () => {
-        return {
-            messageList: [],
-            newMessage: "",
-        }
+  name: "Chat",
+  data: () => {
+    return {
+      messageList: [],
+      newMessage: ""
+    };
+  },
+  methods: {
+    sendMessage() {
+      var message = {
+        date: Date.now(),
+        text: this.newMessage
+      };
+      this.messageList.push(message);
+      this.newMessage = "";
+      this.scrollBottom();
     },
-    methods: {
-        sendMessage() {
-            var message = {
-                date: Date.now(),
-                text: this.newMessage
-            };
-            this.messageList.push(message);
-        }
+    scrollBottom() {
+      var list = this.$refs.chatList;
+      list.scrollTop = list.scrollHeight;
     }
-}
+  },
+  mounted() {
+    var list = this.$refs.chatList;
+    var observer = new MutationObserver(this.scrollBottom);
+    observer.observe(list, { childList: true });
+  }
+};
 </script>
 
 <style scoped>
+.chat-list {
+  height: 600px;
+  overflow: auto;
+  border: 1px solid black;
+}
 </style>
