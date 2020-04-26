6a6babacead4a513a92a7af09dcff077953b74d1
Felix Martinez <f.martinez.espinal@gmail.com>
2020-04-26 09:04:30 -0400

    Adding initial chat Vue component and logic to display messages

---
+++ StockChat.Web/stockchat.web/src/components/Chat.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <div>
+        <ul>
+            <li :key="message.date" v-for="message in messageList">
+                {{ message.text }}
+            </li>
+        </ul>
+    </div>
+    <div>
+        <input v-model="newMessage" v-on:keyup.enter="sendMessage" />
+        <button v-on:click="sendMessage">Send</button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Chat',
+    data: () => {
+        return {
+            messageList: [],
+            newMessage: "",
+        }
+    },
+    methods: {
+        sendMessage() {
+            var message = {
+                date: Date.now(),
+                text: this.newMessage
+            };
+            this.messageList.push(message);
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
