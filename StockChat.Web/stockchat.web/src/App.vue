ac208e05a06e24cfbc6b25ec94a78f9272197b9b
Felix Martinez <f.martinez.espinal@gmail.com>
2020-04-27 08:46:16 -0400

    Adding routing to frontend

--- StockChat.Web/stockchat.web/src/App.vue
+++ StockChat.Web/stockchat.web/src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <Chat />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Chat from './components/Chat'
-
 export default {
-  name: 'App',
-  components: {
-    Chat
-  }
-}
+  name: "App",
+  components: {}
+};
 </script>
 
 <style>
--- StockChat.Web/stockchat.web/src/main.js
+++ StockChat.Web/stockchat.web/src/main.js
@@ -1,12 +1,26 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.min.css";
+import VueRouter from "vue-router";
 import QuestionHub from "./services/messagesHub";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import Chat from "./components/Chat.vue";
+import Login from "./components/Login.vue";
+
+Vue.use(VueRouter);
+Vue.use(QuestionHub);
 Vue.config.productionTip = false;
 
-Vue.use(QuestionHub);
+const routes = [
+  { path: "/chat", component: Chat },
+  { path: "/login", component: Login },
+];
+
+const router = new VueRouter({
+  routes,
+});
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
